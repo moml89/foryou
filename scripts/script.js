7d02f571f73b4d07f01306009675dfb0b97df458
moml89 <moml89@mail.ru>
2022-08-02 18:39:07 +0500

    add files to my repository 02-08-22

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+var myButton = document.querySelector('.buttonClick');
+var myHeading = document.querySelector('.slogan');
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name',myName);
+    myHeading.textContent = 'It`s All For You, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'It`s All For You, ' + storedName;
+}
+myButton.onclick = function() {
+    setUserName();
+}
+
+var myImage = document.querySelector('main > div > div > img'); //выбирает первый "по пути" тег
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/gallery1.jpg') {
+      myImage.setAttribute ('src','images/gallery2.jpg');
+    } else {
+      myImage.setAttribute ('src','images/gallery1.jpg');
+    }
+}
